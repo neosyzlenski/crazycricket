b1191137904498608541e72817fa2733b05ad03d
Kapil Sharma <neosyzlenski@gmail.com>
2016-07-01 15:42:37 +0530

    Adding test cases.

--- src/main/java/com/bfm/acs/crazycricket/rest/CrazyCricketService.java
+++ src/main/java/com/bfm/acs/crazycricket/rest/CrazyCricketService.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,14 +29,14 @@ public class CrazyCricketService {
 	DataStore dataStore;
     
 	@RequestMapping(value = "/leaderboard")
-	public List<Map<String, Long>> getLeaderBoardInRange(
+	public List<Map<String, Long>> getLeaderBoard(
 			@RequestParam(name = "start", required = false) String startDate, 
 			@RequestParam(name = "end", required = false) String endDate){
 		return dataStore.getLeaderBoard(startDate, endDate);
 	}
 	
 	@RequestMapping(value = "/national_leaderboard")
-	public List<Map<String, Long>> getNationalLeaderBoardInRange(
+	public List<Map<String, Long>> getNationalLeaderBoard(
 			@RequestParam(name = "start", required = false) String startDate, 
 			@RequestParam(name = "end", required = false) String endDate){
 		return dataStore.getNationalLeaderBoard(startDate, endDate);
--- src/test/java/com/bfm/acs/crazycricket/CrazyCricketServiceTest.java
+++ src/test/java/com/bfm/acs/crazycricket/CrazyCricketServiceTest.java
@@ -3,18 +3,39 @@
  */
 package com.bfm.acs.crazycricket;
 
-import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.ApplicationContext;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import com.bfm.acs.crazycricket.data.DataStore;
+import com.bfm.acs.crazycricket.kafka.KafkaProcessor;
 import com.bfm.acs.crazycricket.rest.CrazyCricketService;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Test cases for our crazy service
@@ -24,32 +45,41 @@ import com.bfm.acs.crazycricket.rest.CrazyCricketService;
  *
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
+@SpringApplicationConfiguration(classes = { Application.class, TestConfig.class })
 @WebAppConfiguration
 public class CrazyCricketServiceTest {
-    @Mock
-    private DataStore dataStore;
-
-    @Mock
-    private CrazyCricketService crazyCricketService;
+	@InjectMocks
+    private CrazyCricketService service;
     
-    @Test
-    public void testLeaderBoard(){
-    	
+	@Autowired
+    private WebApplicationContext context;
+	
+	private MockMvc mvc;
+	
+	private KafkaProcessor kfp;
+	
+	@Before
+    public void initTests() {
+        MockitoAnnotations.initMocks(this);
+        mvc = MockMvcBuilders.webAppContextSetup(context).build();
+        
+        String[] topics = {"TEST", "TWENTY_TWENTY", "LIMITED_OVERS"};
+    	DataStore dataStore = context.getBean(DataStore.class);
+    	kfp = new KafkaProcessor(dataStore, "localhost:9092", topics);
+    	Thread processor = new Thread(kfp);
+    	processor.start();
+    }
+    
+    @After
+    public void tearDown(){
+    	kfp.stop();
     }
     
     @Test
-    public void testLeaderBoardWithDateRange(){
-    	
-    }
-    
-    @Test
-    public void testNationalLeaderBoard(){
-    	
-    }
-    
-    @Test
-    public void testNationalLeaderBoardWithDateRange(){
-    	
+    public void testNationalLeaderBoard() throws Exception{    	
+    	mvc.perform(get("/api/national_leaderboard")
+                	.accept(MediaType.APPLICATION_JSON))
+                	.andExpect(status().isOk())
+                	.andExpect(jsonPath("$", hasSize(4)));
     }
 }
