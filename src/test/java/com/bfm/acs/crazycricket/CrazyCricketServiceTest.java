dd8d9f3b1a0c25c2d06ebb97fdb694c4d1e2bac3
Kapil Sharma <neosyzlenski@gmail.com>
2016-07-02 04:10:57 +0530

    Updating with test cases.

--- src/test/java/com/bfm/acs/crazycricket/CrazyCricketServiceTest.java
+++ src/test/java/com/bfm/acs/crazycricket/CrazyCricketServiceTest.java
@@ -3,14 +3,7 @@
  */
 package com.bfm.acs.crazycricket;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -19,20 +12,19 @@ import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.bfm.acs.crazycricket.data.DataStore;
-import com.bfm.acs.crazycricket.kafka.KafkaProcessor;
 import com.bfm.acs.crazycricket.rest.CrazyCricketService;
 
 import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -45,7 +37,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  *
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = { Application.class, TestConfig.class })
+@SpringApplicationConfiguration(classes = { Application.class })
 @WebAppConfiguration
 public class CrazyCricketServiceTest {
 	@InjectMocks
@@ -55,31 +47,75 @@ public class CrazyCricketServiceTest {
     private WebApplicationContext context;
 	
 	private MockMvc mvc;
-	
-	private KafkaProcessor kfp;
+	private static EmbeddedDatabase db;
 	
 	@Before
     public void initTests() {
         MockitoAnnotations.initMocks(this);
         mvc = MockMvcBuilders.webAppContextSetup(context).build();
-        
-        String[] topics = {"TEST", "TWENTY_TWENTY", "LIMITED_OVERS"};
-    	DataStore dataStore = context.getBean(DataStore.class);
-    	kfp = new KafkaProcessor(dataStore, "localhost:9092", topics);
-    	Thread processor = new Thread(kfp);
-    	processor.start();
     }
+	
+	@BeforeClass
+	public static void setUp(){
+		db = new EmbeddedDatabaseBuilder()
+				.addScript(new ClassPathResource("schema.sql").getPath())
+				.addScript("test-data.sql")
+				.build();
+	}
+	
+	@AfterClass
+	public static void tearDown(){
+		db.shutdown();
+	}
     
-    @After
-    public void tearDown(){
-    	kfp.stop();
+	@Test
+    public void testLeaderBoard() throws Exception{
+		mvc.perform(get("/api/leaderboard")
+                	.accept(MediaType.APPLICATION_JSON))
+                	.andExpect(status().isOk())
+                	.andExpect(jsonPath("$", hasSize(5)))
+                	.andExpect(jsonPath("$[0]", hasEntry("shubham", 5)))
+                	.andExpect(jsonPath("$[1]", hasEntry("oscar", 4)))
+                	.andExpect(jsonPath("$[2]", hasEntry("sachin", 4)))
+                	.andExpect(jsonPath("$[3]", hasEntry("andrew", 1)))
+                	.andExpect(jsonPath("$[4]", hasEntry("imran", 1)));
+		
     }
-    
+	
+	@Test
+    public void testLeaderBoardWithDateRange() throws Exception{    	
+		mvc.perform(get("/api/leaderboard?start=20160101&end=20160101")
+            	.accept(MediaType.APPLICATION_JSON))
+            	.andExpect(status().isOk())
+            	.andExpect(jsonPath("$", hasSize(5)))
+            	.andExpect(jsonPath("$[0]", hasEntry("oscar", 2)))
+            	.andExpect(jsonPath("$[1]", hasEntry("andrew", 1)))
+            	.andExpect(jsonPath("$[2]", hasEntry("imran", 1)))
+            	.andExpect(jsonPath("$[3]", hasEntry("sachin", 1)))
+            	.andExpect(jsonPath("$[4]", hasEntry("shubham", 1)));
+    }
+	
     @Test
     public void testNationalLeaderBoard() throws Exception{    	
     	mvc.perform(get("/api/national_leaderboard")
-                	.accept(MediaType.APPLICATION_JSON))
-                	.andExpect(status().isOk())
-                	.andExpect(jsonPath("$", hasSize(4)));
+            	.accept(MediaType.APPLICATION_JSON))
+            	.andExpect(status().isOk())
+            	.andExpect(jsonPath("$", hasSize(4)))
+            	.andExpect(jsonPath("$[0]", hasEntry("India", 9)))
+            	.andExpect(jsonPath("$[1]", hasEntry("England", 4)))
+            	.andExpect(jsonPath("$[2]", hasEntry("Pakistan", 1)))
+            	.andExpect(jsonPath("$[3]", hasEntry("USA", 1)));
+    }
+    
+    @Test
+    public void testNationalLeaderBoardWithDateRange() throws Exception{    	
+    	mvc.perform(get("/api/national_leaderboard?start=20160101&end=20160101")
+            	.accept(MediaType.APPLICATION_JSON))
+            	.andExpect(status().isOk())
+            	.andExpect(jsonPath("$", hasSize(4)))
+            	.andExpect(jsonPath("$[0]", hasEntry("England", 2)))
+            	.andExpect(jsonPath("$[1]", hasEntry("India", 2)))
+            	.andExpect(jsonPath("$[2]", hasEntry("Pakistan", 1)))
+            	.andExpect(jsonPath("$[3]", hasEntry("USA", 1)));
     }
 }
