92b0a5a907bf5fe66a148ce144559892a70e1736
Kapil Sharma <neosyzlenski@gmail.com>
2016-07-02 16:12:50 +0530

    Adding run script.

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
+############################################################
+## Triggers the Crazy Cricket REST API and Kafka Processor 
+## Author - Kapil
+## Created - July 2, 2016
+############################################################
+
 java -jar target/crazycricket-SNAPSHOT.jar $1
